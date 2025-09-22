f683e5ce004527ff08d22aec7d2f829f2558fcbe
Matt Gollob <mattness@users.noreply.github.com>
2025-09-22 15:05:13 -0400

    [snacks] Swap Ctrl+b/u and Ctrl+f/d in snacks picker

--- lua/plugins/extend-snacks.lua
+++ lua/plugins/extend-snacks.lua
@@ -3,6 +3,18 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = { enabled = false },
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<c-b>"] = { "list_scroll_up", mode = { "i", "n" } },
+              ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
+              ["<c-f>"] = { "list_scroll_down", mode = { "i", "n" } },
+              ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+            }
+          }
+        }
+      }
     },
   },
 }
