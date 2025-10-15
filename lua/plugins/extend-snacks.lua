df7f7bf5035fe255788e1cfb51d7391248984f2b
Matt Gollob <mattness@users.noreply.github.com>
2025-10-15 16:48:11 -0400

    [snacks] Add window selection when opening from explorer

--- lua/plugins/extend-snacks.lua
+++ lua/plugins/extend-snacks.lua
@@ -4,6 +4,35 @@ return {
     opts = {
       dashboard = { enabled = false },
       picker = {
+        sources = {
+          explorer = {
+            actions = {
+              toggle_or_jump = function(picker, item)
+                if not item then
+                  return
+                end
+                if item.dir then
+                  local Tree = require("snacks.explorer.tree")
+                  local Actions = require("snacks.explorer.actions")
+                  Tree:toggle(item.file)
+                  Actions.update(picker, { refresh = true })
+                else
+                  picker:action("pick_win")
+                  picker:action("jump")
+                end
+              end,
+            },
+            win = {
+              list = {
+                keys = {
+                  ["<CR>"] = "toggle_or_jump",
+                  ["<c-v>"] = { { "pick_win", "edit_vsplit" } },
+                  ["<c-s>"] = { { "pick_win", "edit_split" } },
+                },
+              },
+            },
+          },
+        },
         win = {
           input = {
             keys = {
@@ -11,10 +40,10 @@ return {
               ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
               ["<c-f>"] = { "list_scroll_down", mode = { "i", "n" } },
               ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
-            }
-          }
-        }
-      }
+            },
+          },
+        },
+      },
     },
   },
 }
